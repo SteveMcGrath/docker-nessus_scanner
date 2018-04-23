238aec2f989257328d4658cdbe17e8e02b8e8534
Steven McGrath <steve@chigeek.com>
2018-04-23 12:47:22 -0500

    recoded the nessus.sh script for better handling of input

--- nessus.sh
+++ nessus.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-function link_scanner {
-    if [ -n "${LINKING_KEY}" ];then
+if [ "$(/opt/nessus/sbin/nessuscli fix --list | grep 'do not exist yet' | wc -l)" -ne "0" ];then
+    echo "-- New Nessus Installation, Attempting to license it using the provided configuration"
+    if [ "$(/opt/nessus/sbin/nessuscli managed status | grep 'Linked to' | wc -l)" == "0" ] && [ -n "${LINKING_KEY}" ];then
+        echo "-- Linking Scanner to Tenable.io"
         local args=" --key=${LINKING_KEY}"
         [ -n "${SCANNER_NAME}" ] && args=${args}" --name=${SCANNER_NAME}"
         [ -n "${PROXY_HOST}"   ] && args=${args}" --proxy-host=${PROXY_HOST}"
@@ -13,20 +15,20 @@ function link_scanner {
         [ -n "${MANAGER_PORT}" ] && args=${args}" --port=${MANAGER_PORT}"           || args=${args}" --port=443"
         /opt/nessus/sbin/nessuscli managed link ${args}
     elif [ -n "${LICENSE}" ];then
+        echo "-- Registering as a Nessus Pro scanner"
         /opt/nessus/sbin/nessuscli fetch --register "${LICENSE}"
     elif [ -n "${SECURITYCENTER}" ];then
+        echo "-- Registering as a SecurityCenter-linked scanner"
         /opt/nessus/sbin/nessuscli fetch --security-center
     fi
-}
-
-if [ "$(/opt/nessus/sbin/nessuscli managed status | grep "Linked to" | wc -l)" == "0" ] && [ -n "${LINKING_KEY}" ];then
-    link_scanner
 fi
 
 if [ -n "${ADMIN_USER}" ] && [ -n "${ADMIN_PASS}" ];then
     if [[ ! "$(/opt/nessus/sbin/nessuscli lsuser)" =~ "${ADMIN_USER}" ]];then
+        echo "-- Creating the administrative user based on the provided settings"
         /usr/bin/nessus_adduser.exp "${ADMIN_USER}" "${ADMIN_PASS}" > /dev/null
     fi
 fi 
 
+echo "-- Starting the Nessus service"
 /opt/nessus/sbin/nessus-service
