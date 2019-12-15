416c8dd4279253c29d38f3260e81620d7b4f76a8
Eli Richmond <eerichmond@ucdavis.edu>
2019-12-14 22:52:47 -0800

    fix typos in the NO_ROOT usermod and chown

--- nessus.sh
+++ nessus.sh
@@ -59,7 +59,7 @@ if [ ! -f /opt/nessus/var/nessus/first_run ];then
         echo "-- Reconfiguring Nessus for a non-root configuration..."
         echo "-- Creating user ${NON_PRIV_USER} to run the Nessus services..."
         useradd -r ${NON_PRIV_USER}
-        usermod -a -F tty ${NON_PRIV_USER}
+        usermod -a -G tty ${NON_PRIV_USER}
 
         echo "-- Setting the appropriate permissions for the Nessus services..."
         setcap "cap_net_admin,cap_net_raw,cap_sys_resource+eip" /opt/nessus/sbin/nessusd
@@ -67,7 +67,7 @@ if [ ! -f /opt/nessus/var/nessus/first_run ];then
 
         echo "-- Changing the ownership of the Nessus installation to ${NON_PRIV_USER}"
         chmod 750 /opt/nessus/sbin/*
-        chown -R $USER:$USER /opt/nessus
+        chown -R ${NON_PRIV_USER}:${NON_PRIV_USER} /opt/nessus
     fi
 
     # Create the first_run file so that we know the initialization is complete.
